d6815250983b8346ed96d14ba62ff1bfd1ba2de5
HopsKaese <nils-ollrogge@outlook.de>
2017-07-03 03:00:17 -0700

    Fixed Calculation of Center

--- calc.rb
+++ calc.rb
@@ -2,8 +2,8 @@
 # [1] = long -> Längengrad
 
 def distance loc1, loc2
-  rad_per_deg = Math::PI/180  # PI / 180                # Earth radius in kilometers          # Radius in meters
-  rkm = 6371  
+  rad_per_deg = Math::PI/180  # PI / 180                      
+  rkm = 6371   # Earth radius in kilometers 
   dlat_rad = (loc2[0]-loc1[0]) * rad_per_deg  # Delta, converted to rad
   dlon_rad = (loc2[1]-loc1[1]) * rad_per_deg
 
@@ -13,7 +13,7 @@ def distance loc1, loc2
   a = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad/2)**2
   c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
 
-  rkm * c # Delta in meters
+  rkm * c
 end
 
 def number_or_nil(string)
@@ -70,7 +70,7 @@ cnt = 0
   x = lat1
   y += a2
   (0..circles_hori).each do |j|
-    center_points[cnt] = [x,y]
+    center_points[cnt] = [x+a2/2,y+a2/2]
     x += a2
     cnt += 1
   end
