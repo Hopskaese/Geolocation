e20336d99ac5afae9f5033d23a401366b4ac3efd
Nils-Ollrogge <nils-po@hotmail.de>
2017-07-02 17:07:35 -0700

    My Brain hurts

--- calc.rb
+++ calc.rb
@@ -1,9 +1,9 @@
+# [0] = lat -> Breitengrad
+# [1] = long -> Längengrad
 
 def distance loc1, loc2
-  rad_per_deg = Math::PI/180  # PI / 180
-  rkm = 6371                  # Earth radius in kilometers
-  rm = rkm * 1000             # Radius in meters
-
+  rad_per_deg = Math::PI/180  # PI / 180                # Earth radius in kilometers          # Radius in meters
+  rkm = 6371  
   dlat_rad = (loc2[0]-loc1[0]) * rad_per_deg  # Delta, converted to rad
   dlon_rad = (loc2[1]-loc1[1]) * rad_per_deg
 
@@ -13,7 +13,7 @@ def distance loc1, loc2
   a = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad/2)**2
   c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
 
-  rm * c # Delta in meters
+  rkm * c # Delta in meters
 end
 
 def number_or_nil(string)
@@ -21,7 +21,6 @@ def number_or_nil(string)
 rescue ArgumentError
   nil
 end
-
 
 if(ARGV.length < 5)
 	abort("Not enough arguments provided")
@@ -41,17 +40,38 @@ if(lat1 == nil || long1 == nil || lat2 == nil || long2 == nil || radius == nil)
 end
 
 #calculating sides
-loc1 = Array.new(lat1, long1)
-loc2 = Array.new(lat2, long1)
+loc1 = Array[lat1, long1]
+loc2 = Array[lat2, long1]
 
-loc3 = Array.new(lat1, long1)
-loc4 = Array.new(lat1, long2)
-
-test1 = Array.new(38.0, 50.0)
-test2 = Array.new(38.0, 70.0)
+loc3 = Array[lat1, long1]
+loc4 = Array[lat1, long2]
 
 a = distance(loc1, loc2)
 b = distance(loc3, loc4)
-c = distance(test1, test2)
-c /= 1000
-puts "#{c}"
+
+#Calculating max square inside circle
+a2 = radius * Math.sqrt(2)
+
+circles_hori = a / a2
+circles_verti = b / a2
+
+circles_hori = circles_hori.ceil
+circles_verti = circles_verti.ceil
+
+center_points = Hash.new(Array.new)
+
+a2 = (a2 / 6371) * (180 / Math::PI)
+
+x = lat1
+y = long1 - a2
+
+(1..circles_verti).each do |i|
+  x = lat1
+  y += a2
+  (1..circles_hori).each do |j|
+    center_points[i*j] = [x,y]
+    x += a2
+  end
+end
+
+puts center_points
